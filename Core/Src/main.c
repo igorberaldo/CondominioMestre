160b5b1dd2e2d531d99db8723ff74bc2e326f3d8
Igor Beraldo <beraldo_igor@hotmail.com>
2021-02-28 01:05:15 -0300

    adding functions to access the flash memory

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -44,7 +44,8 @@
 #define W5500_rx() W5500_rxtx(0xff)
 #define W5500_tx(data) W5500_rxtx(data)
 
-uint8_t W5500_rxtx(uint8_t data);
+#define FLASH_STORAGE 0x08005000
+#define page_size 0x800
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -130,6 +131,7 @@ void StartReadInput3(void *argument);
 void StartReadInput4(void *argument);
 
 /* USER CODE BEGIN PFP */
+uint8_t W5500_rxtx(uint8_t data);
 void  wizchip_select(void);
 void  wizchip_deselect(void);
 void  wizchip_write(uint8_t wb);
@@ -137,6 +139,8 @@ uint8_t wizchip_read();
 void network_init(void);								// Initialize Network information and display it
 int32_t tcp_http_mt(uint8_t, uint8_t*, uint16_t);		// Multythread TCP server
 void HTTP_reset(uint8_t sockn);
+void read_flash(uint8_t* data);		//Reads data from the flash memory
+void save_to_flash(uint8_t* data);		//Writes data to the flash memory
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -697,6 +701,68 @@ int32_t tcp_http_mt(uint8_t sn, uint8_t* buf, uint16_t port)
          break;
    }
    return 1;
+}
+
+void save_to_flash(uint8_t *data)
+{
+	volatile uint32_t data_to_FLASH[(strlen((char*)data)/4)	+ (int)((strlen((char*)data) % 4) != 0)];
+	memset((uint8_t*)data_to_FLASH, 0, strlen((char*)data_to_FLASH));
+	strcpy((char*)data_to_FLASH, (char*)data);
+
+	volatile uint32_t data_length = (strlen((char*)data_to_FLASH) / 4)
+									+ (int)((strlen((char*)data_to_FLASH) % 4) != 0);
+	volatile uint16_t pages = (strlen((char*)data)/page_size)
+									+ (int)((strlen((char*)data)%page_size) != 0);
+	  /* Unlock the Flash to enable the flash control register access *************/
+	  HAL_FLASH_Unlock();
+
+	  /* Allow Access to option bytes sector */
+	  HAL_FLASH_OB_Unlock();
+
+	  /* Fill EraseInit structure*/
+	  FLASH_EraseInitTypeDef EraseInitStruct;
+	  EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
+	  EraseInitStruct.PageAddress = FLASH_STORAGE;
+	  EraseInitStruct.NbPages = pages;
+	  uint32_t PageError;
+
+	  volatile uint32_t write_cnt=0, index=0;
+
+	  volatile HAL_StatusTypeDef status;
+	  status = HAL_FLASHEx_Erase(&EraseInitStruct, &PageError);
+	  while(index < data_length)
+	  {
+		  if (status == HAL_OK)
+		  {
+			  status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, FLASH_STORAGE+write_cnt, data_to_FLASH[index]);
+			  if(status == HAL_OK)
+			  {
+				  write_cnt += 4;
+				  index++;
+			  }
+		  }
+	  }
+
+	  HAL_FLASH_OB_Lock();
+	  HAL_FLASH_Lock();
+}
+
+void read_flash(uint8_t* data)
+{
+	volatile uint32_t read_data;
+	volatile uint32_t read_cnt=0;
+	do
+	{
+		read_data = *(uint32_t*)(FLASH_STORAGE + read_cnt);
+		if(read_data != 0xFFFFFFFF)
+		{
+			data[read_cnt] = (uint8_t)read_data;
+			data[read_cnt + 1] = (uint8_t)(read_data >> 8);
+			data[read_cnt + 2] = (uint8_t)(read_data >> 16);
+			data[read_cnt + 3] = (uint8_t)(read_data >> 24);
+			read_cnt += 4;
+		}
+	}while(read_data != 0xFFFFFFFF);
 }
 /* USER CODE END 4 */
 
